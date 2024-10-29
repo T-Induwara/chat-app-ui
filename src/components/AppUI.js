052e5ded35b47785a0bd297cfc9a3490ae460e39
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-10-29 15:19:01 +0530

    Imported required images

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Avatar from '../assets/images/avatar.jpg';
+import DogPic1 from '../assets/images/dog-image-1.jpg';
+import DogPic2 from '../assets/images/dog-image-2.jpg';
+import DogPic3 from '../assets/images/dog-image-3.jpg';
+
+const AppUI = () => {
+    return <div className="app">
+
+    </div>
+};
+export default AppUI;
