99ac433b078d4fa2aec9834880cdfa579e2975eb
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-12-21 18:24:26 +0530

    Made minor changes

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -7,9 +7,19 @@ import DogPic2 from '../assets/images/dog-image-2.jpg';
 import DogPic3 from '../assets/images/dog-image-3.jpg';
 
 const AppUI = () => {
-    return <div className="app">
+    return <div className="d-flex flex-row app">
         <div className="top-shape"></div>
         <div className="bottom-shape"></div>
+        <div className="container app-wrapper">
+            <div className="row d-flex wrapper-row">
+                <div className="app-border">
+                    <div className="app-ui"></div>
+                </div>
+                <div className="text-wrapper">
+
+                </div>
+            </div>
+        </div>
     </div>
 };
 export default AppUI;
