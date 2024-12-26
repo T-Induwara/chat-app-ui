268bcb81803d9215ba2248aa9e188b37e3a35d5e
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-12-26 21:16:54 +0530

    Added all the main content to the appUI.js file

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import BackIcon from '../assets/images/back-icon.png';
+import SendIcon from '../assets/images/send-icon.png';
 import Avatar from '../assets/images/avatar.jpg';
 import DogPic1 from '../assets/images/dog-image-1.jpg';
 import DogPic2 from '../assets/images/dog-image-2.jpg';
@@ -38,7 +39,58 @@ const AppUI = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="ui-content"></div>
+                            <div className="ui-content">
+                                <div className="content-col">
+                                    <p className="left-chat">
+                                        That sounds great. I'd be happy with that.
+                                    </p>
+                                    <p className="left-chat">
+                                        Could you send over some pictures of your dog, please?
+                                    </p>
+                                </div>
+                                <div className="content-col">
+                                    <div className="d-flex flex-row pet-images">
+                                        <img src={DogPic1} className="dog-img" alt="Dog image"/>
+                                        <img src={DogPic2} className="dog-img" alt="Dog image"/>
+                                        <img src={DogPic3} className="dog-img" alt="Dog image"/>
+                                    </div>
+                                    <p className="right-chat">
+                                        Here are a few picture. She's a happy girl!
+                                    </p>
+                                    <p className="right-chat">
+                                        Can you make it?
+                                    </p>
+                                </div>
+                                <div className="content-col">
+                                    <p className="left-chat">
+                                        She looks so happy! The time we discussed works. How long shall I take her out for?
+                                    </p>
+                                    <div className="d-flex flex-row left-chat-check">
+                                        <div className="d-flex flex-row">
+                                            <input type="radio" id="selection" name="time-walk" value="30"/>
+                                            <p class="user-choice-text">
+                                                30 minute walk
+                                            </p>
+                                        </div>
+                                        <p className="choice-price">$29</p>
+                                    </div>
+                                    <div className="d-flex flex-row left-chat-check">
+                                        <div className="d-flex flex-row">
+                                            <input type="radio" id="selection" name="time-walk" value="30"/>
+                                            <p className="user-choice-text">
+                                                1 hour walk
+                                            </p>
+                                        </div>
+                                        <p className="choice-price">$49</p>
+                                    </div>
+                                </div>
+                                <div className="d-flex flex-row content-col">
+                                    <input type="text" placeholder="Type a message..." className="text-placeholder"></input>
+                                    <div className="send-btn">
+
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
