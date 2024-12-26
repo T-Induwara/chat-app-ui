1ae1375471eaf487086c88975d7a45ab6a5a36cc
Tharusha Induwara <tharusha.induwar@gmail.com>
2024-12-26 17:46:12 +0530

    Done rough styling for img col

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import BackIcon from '../assets/images/back-icon.png';
 import Avatar from '../assets/images/avatar.jpg';
 import DogPic1 from '../assets/images/dog-image-1.jpg';
 import DogPic2 from '../assets/images/dog-image-2.jpg';
@@ -16,8 +17,24 @@ const AppUI = () => {
                     <div className="app-ui">
                         <div className="app-body d-flex flex-column">
                             <div className="ui-notch"></div>
-                            <div className="ui-header">
-                                
+                            <div className="d-flex flex-column ui-header">
+                                <div className="header-col"></div>
+                                {/*Below div is for app header contents*/}
+                                <div className="d-flex flex-row header-col"> 
+                                    <div className="d-flex flex-row img-col">
+                                        <img src={BackIcon} className="back-icon" alt="Back icon"></img>
+                                        <img src={Avatar} className="user-img" alt="User avatar"></img>
+                                    </div>
+                                    <div className="d-flex flex-column data-col">
+                                        <p className="username">Samuel Green</p>
+                                        <p className="user-status">Available to Walk</p>
+                                    </div>
+                                    <div className="d-flex flex-column menu-col">
+                                        <div className="menu-content"></div>
+                                        <div className="menu-content"></div>
+                                        <div className="menu-content"></div>
+                                    </div>
+                                </div>
                             </div>
                             <div className="ui-content"></div>
                         </div>
